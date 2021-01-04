e6af244aefe005c250afb06a56db115111ebc749
Lukas Kofoed Petersen <sonaudio1@gmail.com>
2021-01-04 17:58:07 +0100

    Updating

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,36 @@
 package controller;
 
-public class Controller {//lmao det virker
+import java.io.FileInputStream;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class Controller extends Application {
+	
+	public static void main(String[] args) {
+	// inside JVM launch method calls start method
+		Application.launch(args);
+	}
+	
+	public void start(Stage outStage) throws IOException {
+		// Setting title to screen
+		outStage.setTitle("FXML Controller Button with CSS Styles");
+		// Creating FXML Loader instance
+		FXMLLoader loader = new FXMLLoader();
+		// FXML path
+		String fxmlActualPath = "C://Users//paramesh//Desktop//Desktop//Verinon Purpose//FXMLController//src//com//fxml//controller/FXMLLabelAddingButtonController.fxml";
+		// Setting FXML path
+		FileInputStream fxmlStream = new FileInputStream(fxmlActualPath);
+		// Creating VBox to add FXML label and text fields
+		VBox vBox = (VBox) loader.load(fxmlStream);
+		// Creating scene
+		Scene screen = new Scene(vBox, 500, 500);
+		// Setting screen stage
+		outStage.setScene(screen);
+		// Showing the screen
+		outStage.show();
+	}
 }
--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,5 +1,26 @@
 package gui;
 
-public class MainFrame {
+public class MainFrame extends JFrame{
+	private static final long serialVersionUID = 1L;
+	
+	public MainFrame() {
+		super("Swing Demo");
+		setJMenuBar(createMenu());
+		setSize(1000, 800);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+	
+	private JMenuBar createMenu() {
+		var menuBar = new JMenuBar();
+		var fileMenu = new JMenu("File");
+		var exitItem = new JMenuItem("Exit");
+		
+		exitItem.addActionListener(e -> System.exit(0));
+		
+		fileMenu.add(exitItem);
+		menuBar.add(fileMenu);
+		return menuBar;
+	}
 
 }
