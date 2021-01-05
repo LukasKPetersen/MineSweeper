3b0648fc5ee0e6f7c511a57ca3e39051cd9ac095
s204487 <s204487@student.dtu.dk>
2021-01-05 16:43:14 +0100

    Ny view og controller class samt pictures mappe

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,74 +1,92 @@
-<<<<<<< HEAD
-package controller;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-public class Controller extends Application {
-	
-	public static void main(String[] args) {
-	// inside JVM launch method calls start method
-		Application.launch(args);
-	}
-	
-	public void start(Stage outStage) throws IOException {
-		// Setting title to screen
-		outStage.setTitle("FXML Controller Button with CSS Styles");
-		// Creating FXML Loader instance
-		FXMLLoader loader = new FXMLLoader();
-		// FXML path
-		String fxmlActualPath = "C://Users//paramesh//Desktop//Desktop//Verinon Purpose//FXMLController//src//com//fxml//controller/FXMLLabelAddingButtonController.fxml";
-		// Setting FXML path
-		FileInputStream fxmlStream = new FileInputStream(fxmlActualPath);
-		// Creating VBox to add FXML label and text fields
-		VBox vBox = (VBox) loader.load(fxmlStream);
-		// Creating scene
-		Scene screen = new Scene(vBox, 500, 500);
-		// Setting screen stage
-		outStage.setScene(screen);
-		// Showing the screen
-		outStage.show();
-	}
-=======
 package src.controller;
-import java.util.Timer;
-import java.util.TimerTask;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.scene.Group;
+
+import javafx.event.*;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.event.ActionEvent;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.event.EventHandler;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
-import javafx.scene.input.KeyEvent;
-import java.util.*;
-import java.awt.Graphics;
-import java.io.IOException;
-import javafx.scene.layout.GridPane;
 
 public class Controller {
-	
->>>>>>> f715c0bf86201a31c3ed1a03ce0c5c428c32d2ee
+
+	/*
+	 * @SuppressWarnings("unused") private CounterModel counterModel;
+	 * 
+	 * @SuppressWarnings("unused") private CounterView counterView;
+	 */
+	private EventHandler<MouseEvent> eventMouse;
+	private int posMousePressedX;
+	private int posMousePressedY;
+
+	public Controller(int tilesize) {
+
+		this.setEventMousePressed(new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent event) {
+		
+				double x = event.getX();
+				double y = event.getY();
+				int xCoorTiles = 0;
+				int yCoorTiles = 0;
+				while (x - tilesize > 0) {
+					x = x - tilesize;
+					xCoorTiles++;
+				}
+				while (y - tilesize > 0) {
+					y = y - tilesize;
+					yCoorTiles++;
+				}
+				
+				if (event.getButton().toString() == "PRIMARY") {
+					if (event.getEventType().toString() == "MOUSE_PRESSED") {
+						posMousePressedY = yCoorTiles;
+						posMousePressedX = xCoorTiles;
+						System.out.print("mouse pressed at: ");
+					}
+					if (event.getEventType().toString() == "MOUSE_RELEASED") {
+						if (falseClick(yCoorTiles,xCoorTiles)) {
+							//System.out.print("False click and");
+						} else {
+							//System.out.print("Real click and");
+						}
+						System.out.print("mouse released at:  ");
+						
+					}
+				}
+				if (event.getButton().toString() == "SECONDARY") {
+					if (event.getEventType().toString() == "MOUSE_PRESSED") {
+						posMousePressedY = yCoorTiles;
+						posMousePressedX = xCoorTiles;
+					}
+					if (event.getEventType().toString() == "MOUSE_RELEASED") {
+						if (falseClick(yCoorTiles,xCoorTiles)) {
+							//System.out.print("False click and");
+						} else {
+							//System.out.print("Real click and");
+						}
+					}
+					
+					System.out.print("set flag at: ");
+				}
+				System.out.println(yCoorTiles + "," + xCoorTiles);
+			}
+		});
+
+	}
+	public boolean falseClick(int y, int x) {
+		if (posMousePressedY==y && posMousePressedX==x) {
+			System.out.print("real click and ");
+			return false;
+		} else {
+			System.out.print("false click and ");
+		return true;
+		}
+	}
+
+	public EventHandler<MouseEvent> getEventHandler() {
+		return eventMouse;
+	}
+
+	public void setEventMousePressed(EventHandler<MouseEvent> eventHandler) {
+		this.eventMouse = eventHandler;
+	}
 }
--- src/gui/View.java
+++ src/gui/View.java
@@ -1,35 +1,28 @@
 package src.gui;
-import java.util.Timer;
-import java.util.TimerTask;
+
+
+
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import src.controller.Controller;
+import src.model.Tile;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Random;
+
+import javafx.scene.Scene;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
-import javafx.event.ActionEvent;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.event.EventHandler;
-import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
-import javafx.scene.input.KeyEvent;
-import java.util.*;
-import java.awt.Graphics;
-import java.io.IOException;
-import javafx.scene.layout.GridPane;
+
 
 public class View extends Application {
 	private GraphicsContext gc;
@@ -38,25 +31,58 @@ public class View extends Application {
 	private int amountTilesLength;
 	private int height;
 	private int length;
-	private int tilesize = 30;
+	private int tilesize;
+	private int pressedTileX;
+	private int pressedTileY;
+	private Controller controller = new Controller(tilesize);
+	private Image Bombimage;
+	private Image Buttonimage;
+	private Image Flagimage;
+	private Group root;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Application.launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		//for fun Random tiles game: 
+		Tile[][] tilearr = new Tile[20][10];
+		Random random = new Random();
+		for (int i=0; i<tilearr.length; i++) {
+			for (int j=0; j<tilearr[0].length; j++) {
+				tilearr[i][j] = new Tile(
+						random.nextBoolean(), 
+						random.nextBoolean(), 
+						random.nextBoolean()
+						);
+			}
+		}
+		
+		
+		
+
 		primaryStage.setTitle("MineDraw");
-		Group root = new Group();
+
+		this.root = new Group();
+		
+		this.Bombimage = new Image(new FileInputStream("Pictures\\Bomb.png"));
+		this.Buttonimage = new Image(new FileInputStream("Pictures\\Button.png"));
+		this.Flagimage = new Image(new FileInputStream("Pictures\\Flag.png"));
 
 		this.amountTilesLength = 10;
 		this.amountTilesHeight = 20;
+		this.tilesize=30;
 
+		
 		this.height = tilesize * amountTilesHeight;
 		this.length = tilesize * amountTilesLength;
 
 		Canvas canvas = new Canvas(length, height);
 		gc = canvas.getGraphicsContext2D();
+		gc.setFill(Color.DARKGRAY);
+		gc.fillRect(0, 0, length, height);
 
 		gc.setLineWidth(1);
 		gc.setStroke(Color.GRAY);
@@ -69,41 +95,136 @@ public class View extends Application {
 		GridPane gridPane = new GridPane();
 		gridPane.setMinSize(amountTilesHeight, amountTilesLength);
 
-		root.getChildren().add(canvas);
-		root.getChildren().add(gridPane);
+		this.root.getChildren().add(canvas);
+		this.root.getChildren().add(gridPane);
 		Scene scene = new Scene(root);
 
-		scene.setOnMouseClicked(this::handleMouseEvent);
-		
+		scene.setOnMousePressed(controller.getEventHandler());
+		scene.setOnMouseReleased(controller.getEventHandler());
+		// scene.setOnMousePressed(this::handleMousePressed);
+		// scene.setOnMouseReleased(this::handleMouseReleased);
 
+		//for fun random tiles game
+		for (int i=0; i<amountTilesHeight; i++) {
+			for (int j=0; j<amountTilesLength; j++) {
+				if (tilearr[i][j].hasFlag()) {
+					drawButton(i,j);
+					drawFlag(i,j);	
+				} else if (tilearr[i][j].hasBomb()){
+					drawBomb(i,j);
+				} else if (!tilearr[i][j].isCleared()) {
+					drawButton(i,j);
+				}				
+				//drawButton(i,j);
+				//drawNumber(i,j,2);
+				//drawBomb(i,j);
+			}
+		}
+		
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
-
+	}
+	
+	
+	private void drawNumber(int y, int x, int number) { //coordinates given in arraytiles
+		Text text = new Text();
+		text.setText(Integer.toString(number));
+		text.setX(x*tilesize+tilesize/2);// Calculate by desired tile + half tile
+		text.setY(y*tilesize+tilesize/2);// Calculate by desired tile + half tile
+		text.setFill(Color.BLACK);
+		text.setFont(Font.font((24.1/30)*tilesize));
+		text.setX(text.getX() - text.getLayoutBounds().getWidth() / 2);
+		text.setY(text.getY() + text.getLayoutBounds().getHeight() / 4);
+		this.root.getChildren().add(text);
 	}
 
-	private void handleMouseEvent(MouseEvent event) {
+	private void drawBomb(int y, int x) {//coordinates given in arraytiles
+		ImageView Bomb = new ImageView(Bombimage);
+		Bomb.setX(x*tilesize);// Calculate by desired tile + 1/6 tile
+		Bomb.setY(y*tilesize);// Calculate by desired tile + 1/6 tile
+		Bomb.setFitHeight(tilesize);// Calculate by desired tile + 2/3 tile
+		Bomb.setFitWidth(tilesize);
+		//Bomb.setX(Bomb.getX() - Bomb.getLayoutBounds().getWidth() / 2);
+		//Bomb.setY(Bomb.getY() - Bomb.getLayoutBounds().getHeight() / 2);
+		//Bomb.setFitHeight(tilesize);
+		//Bomb.setFitWidth(tilesize);
+		this.root.getChildren().add(Bomb);
+	}
+
+	private void drawButton(int y, int x) {//coordinates given in arraytiles
+		ImageView Button = new ImageView(Buttonimage);
+		Button.setX(x*tilesize);
+		Button.setY(y*tilesize);
+		Button.setFitHeight(tilesize);
+		Button.setFitWidth(tilesize);
+		this.root.getChildren().add(Button);
+	}
+
+	private void drawFlag(int y, int x) {//coordinates given in arraytiles
+		ImageView Flag = new ImageView(Flagimage);
+		Flag.setX(x*tilesize);
+		Flag.setY(y*tilesize);
+		Flag.setFitHeight(tilesize);
+		Flag.setFitWidth(tilesize);
+		this.root.getChildren().add(Flag);
+	}
+
+	private void handleMouseReleased(MouseEvent event) {
 
 		// get mineArray
 
 		// sender det mouseevent videre til controller klassen
-		
-		
-		//Laver mouseevent om til x og y coordinater ifht. tiles 
-		//NOTE: y aksen er søjlen af tiles helt til venstre og X aksen er rækken af tiles i bunden 
-		//Skal smides over i Controller
+
+		// Laver mouseevent om til x og y coordinater ifht. tiles
+		// NOTE: y aksen er søjlen af tiles helt til venstre og X aksen er rækken af
+		// tiles i bunden
 		double x = event.getX();
 		double y = event.getY();
 		int xCoorTiles = 0;
 		int yCoorTiles = 0;
 		while (x - tilesize > 0) {
-			x=x-tilesize;
+			x = x - tilesize;
 			xCoorTiles++;
 		}
 		while (y - tilesize > 0) {
-			y=y-tilesize;
+			y = y - tilesize;
 			yCoorTiles++;
 		}
+		gc.setFill(Color.WHITE);
+		gc.fillRect(pressedTileX * tilesize, pressedTileY * tilesize, tilesize, tilesize);
+		gc.setStroke(Color.GREY);
+		gc.strokeRect(pressedTileX * tilesize, pressedTileY * tilesize, tilesize, tilesize);
+		System.out.println(yCoorTiles + "," + xCoorTiles);
+	}
+
+	private void handleMousePressed(MouseEvent event) {
+
+		// get mineArray
+
+		// sender det mouseevent videre til controller klassen
+
+		// Laver mouseevent om til x og y coordinater ifht. tiles
+		// NOTE: y aksen er søjlen af tiles helt til venstre og X aksen er rækken af
+		// tiles i bunden
+
+		double x = event.getX();
+		double y = event.getY();
+		int xCoorTiles = 0;
+		int yCoorTiles = 0;
+		while (x - tilesize > 0) {
+			x = x - tilesize;
+			xCoorTiles++;
+		}
+		while (y - tilesize > 0) {
+			y = y - tilesize;
+			yCoorTiles++;
+		}
+		// Only relevant until pressedTile is in Model class
+		this.pressedTileX = xCoorTiles;
+		this.pressedTileY = yCoorTiles;
+		gc.setFill(Color.GREY);
+		gc.fillRect(pressedTileX * tilesize, pressedTileY * tilesize, tilesize, tilesize);
 		System.out.println(yCoorTiles + "," + xCoorTiles);
 	}
 
