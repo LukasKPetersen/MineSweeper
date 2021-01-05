78cf0f51422ccfb4f0f55a7541c66a00469c32e0
unknown <andreasoesterby@hotmail.com>
2021-01-05 14:23:28 +0100

    Tile-klassen

--- src/model/Tile.java
+++ src/model/Tile.java
@@ -2,4 +2,61 @@ package src.model;
 
 public class Tile {
 
+	private boolean bomb;
+	private boolean flag;
+	private boolean cleared;
+
+	// Standard constructor
+	public Tile() {
+		// Initialize to neutral field.
+		this.bomb = false;
+		this.flag = false;
+		this.cleared = false;
+	}
+
+	// Specific field construction
+	public Tile(boolean bomb, boolean flag, boolean cleared) {
+		this.bomb = bomb;
+		this.flag = flag;
+		this.cleared = cleared;
+	}
+
+	// Get-methods
+	public boolean hasBomb() {
+		return this.bomb;
+	}
+
+	public boolean hasFlag() {
+		return this.flag;
+	}
+
+	public boolean isCleared() {
+		return this.cleared;
+	}
+
+	// Set-methods
+	public void setBomb() {
+		this.bomb = true;
+	}
+
+	public void setFlag() {
+		this.flag = !this.flag;
+	}
+
+	public void clearField() {
+		this.cleared = true;
+	}
+
+	public String toString() {
+		if (this.bomb && this.flag) {
+			return "FB";
+		} else if (this.bomb) {
+			return "B";
+		} else if (this.flag) {
+			return "F";
+		} else {
+			return "-";
+		}
+
+	}
 }
